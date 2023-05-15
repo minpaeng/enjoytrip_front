197f597514d6081c92a7d732792cb4fc698e3033
권민정 <22alswjd@naver.com>
2023-05-15 17:54:26 +0900

    refactor: 페이징 수정

--- pass-pj/src/views/InformationView.vue
+++ pass-pj/src/views/InformationView.vue
@@ -4,14 +4,28 @@
     <div class="row">
       <div class="row text-center">
         <div class="col m-5">
-          <h1 style="font-weight: bold; text-decoration-line: underline; text-decoration-thickness: 6px; text-decoration-color: rgb(189, 201, 255)">
+          <h1
+            style="
+              font-weight: bold;
+              text-decoration-line: underline;
+              text-decoration-thickness: 6px;
+              text-decoration-color: rgb(189, 201, 255);
+            "
+          >
             공지사항
           </h1>
         </div>
       </div>
       <div class="row justify-content-between ps-2 pe-2 ms-2 ms-2">
         <div class="col-auto">
-          <a type="button" id="write-button" class="btn btn-outline-primary" style="font-weight: bold" href="" @click="$router.push('/information/write')">
+          <a
+            type="button"
+            id="write-button"
+            class="btn btn-outline-primary"
+            style="font-weight: bold"
+            href=""
+            @click="$router.push('/information/write')"
+          >
             글쓰기
           </a>
         </div>
@@ -19,14 +33,34 @@
           <form id="form-search">
             <input type="hidden" name="pgno" value="1" />
             <div class="row pe-2 me-2">
-              <select v-model="key" class="col form-select me-1" name="key" id="key" aria-label=".form-select example">
+              <select
+                v-model="key"
+                class="col form-select me-1"
+                name="key"
+                id="key"
+                aria-label=".form-select example"
+              >
                 <option selected value="">검색조건</option>
                 <option value="info_board_id">글번호</option>
                 <option value="title">제목</option>
                 <option value="user_id">작성자</option>
               </select>
-              <input type="text" name="word" id="word" placeholder="검색어입력..." class="col form-control me-1" v-model="word" />
-              <button type="button" id="btn-search" class="col btn btn-outline-secondary me-2" @click="getInfoPosts(1, key, word)">검색</button>
+              <input
+                type="text"
+                name="word"
+                id="word"
+                placeholder="검색어입력..."
+                class="col form-control me-1"
+                v-model="word"
+              />
+              <button
+                type="button"
+                id="btn-search"
+                class="col btn btn-outline-secondary me-2"
+                @click="getInfoPosts(1, key, word)"
+              >
+                검색
+              </button>
             </div>
           </form>
         </div>
@@ -44,10 +78,12 @@
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="post in list" :key="post.id">
+                <tr v-for="post in list" :key="post.id" v-bind="list">
                   <td>{{ post.id }}</td>
                   <td>
-                    <router-link :to="{ name: 'informationDetail', params: { no: post.id } }">{{ post.title }}</router-link>
+                    <router-link :to="{ name: 'informationDetail', params: { no: post.id } }">{{
+                      post.title
+                    }}</router-link>
                   </td>
                   <td>{{ post.userId }}</td>
                   <td>{{ post.registerDate }}</td>
@@ -60,19 +96,19 @@
           <div class="post-nav">
             <ul class="pagination justify-content-center">
               <li class="page-item" data-pg="1">
-                <a href="#" class="page-link" @click="$router.push('/information?pgno=1')">최신</a>
+                <a href="#" class="page-link" @click="changePage(1)">최신</a>
               </li>
               <li class="page-item" data-pg="currentPage">
-                <a href="#" class="page-link">이전</a>
+                <a href="#" class="page-link" @click="canGoPrePage">이전</a>
               </li>
               <li v-for="index in pageCount" :key="index" class="page-item" :data-pg="index">
-                <a href="" class="page-link" @click="$router.push(`/information?pgno=${index}`)">{{ index }}</a>
+                <a href="#" class="page-link" @click="changePage(`${index}`)">{{ index }}</a>
               </li>
               <li class="page-item" data-pg="currentPage">
-                <a href="#" class="page-link">다음</a>
+                <a href="#" class="page-link" @click="canGoNextPage">다음</a>
               </li>
               <li class="page-item" data-pg="pageCount">
-                <a href="#" class="page-link" @click="$router.push(`/information?pgno=${pageCount}`)">마지막</a>
+                <a href="#" class="page-link" @click="changePage(pageCount)">마지막</a>
               </li>
             </ul>
           </div>
@@ -93,7 +129,6 @@ export default {
   components: {},
   data() {
     return {
-      currentPage: 1,
       key: "",
       word: "",
       pageCount: null,
@@ -102,7 +137,6 @@ export default {
     };
   },
   created() {
-    this.pgno = this.$route.query.pgno;
     this.getInfoPosts(this.pgno, "", "");
   },
   methods: {
@@ -120,6 +154,22 @@ export default {
       } catch (err) {
         console.log("공지사항 게시글 목록 조회 오류: " + err);
       }
+    },
+    canGoNextPage() {
+      if (this.pgno >= 1 && this.pgno < this.pageCount) {
+        this.pgno++;
+        this.list = this.getInfoPosts(this.pgno, this.key, this.word);
+      }
+    },
+    canGoPrePage() {
+      if (this.pgno > 1 && this.pgno <= this.pageCount) {
+        this.pgno--;
+        this.list = this.getInfoPosts(this.pgno, this.key, this.word);
+      }
+    },
+    changePage(index) {
+      this.pgno = index;
+      this.list = this.getInfoPosts(this.pgno, this.key, this.word);
     },
   },
 };
